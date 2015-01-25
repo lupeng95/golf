ec8e2bdd8cd997540a5155662cf0e5ac23043869
pyunqi <ypang@tibco-support.com>
2015-01-25 14:55:32 +0800

    update userinfo

--- client/account/userinfo.js
+++ client/account/userinfo.js
@@ -1,13 +1,24 @@
 
 var id;
 var user;
+var _status = "保密,未婚,已婚";
+var _sex = "保密,男,女";
+var _position= "未知,主管,经理,总监,副总裁,总裁,首席执行官,董事长,职员";
+var _profession= "未知,金融,互联网/IT,商务服务,贸易,交通/运输,文化/传媒/娱乐/体育,政府,房地产,其他";
+var _driver = "未知,TaylorMade,Titlelist,Callaway,Honma,Mizuno,XXIO,Nike,Srixon,PING,Cleveland,Adams,Maruman,Wilson,MacGregor,KATANA SWORD,KASCO,Nickent,YONEX,George Spirits,Cobra,GIII,PRGR,DUNLOP,EPON,Tourstage,WILLIAMS";
+var _fairway_wood = "未知,TaylorMade,Titlelist,Callaway,Honma,Mizuno,XXIO,Cleveland,Nike,Adams,Maruman,Srixon,PING,Wilson,MacGregor,KATANA SWORD,YONEX,Nickent,George Spirits,Cobra,KASCO,GIII,PRGR,DUNLOP,EPON,Tourstage,WILLIAMS";
+var _hybrid = "未知,TaylorMade,Titlelist,Callaway,Honma,Mizuno,XXIO,Nike,Adams,Srixon,PING,MacGregor,Yamaha,KASCO,GIII,Tourstage,PRGR,EPON,Nickent,WILLIAMS";
+var _irons = "未知,TaylorMade,Titlelist,Callaway,Honma,Mizuno,XXIO,Maruman,PING,Adams,Nike,Srixon,MacGregor,Wilson,Cleveland,Yamaha,KASCO,GIII,Tourstage,PRGR,EPON,Scrach,Nickent,WILLIAMS,YONEX";
+var _wedges="未知,Titlelist,TaylorMade,Cleveland,Callaway,Honma,Mizuno,XXIO,Maruman,Nike,PING,Adams,S-Yard,Srixon,Cobra,Wilson,MacGregor,Tourstage,PRGR,EPON,Scrach,KASCO,Nickent,WILLIAMS,YONEX";
+var _putter ="未知,TaylorMade,Titlelist,PING,Odyssey,Honma,Mizuno,XXIO,Nike,Yes!,Cleveland,Adams,MacGregor,Bettinardi,Maruman,Cobra,Wilson,Srixon,S-Yard,Tourstage,PRGR,EPON,Scrach,KASCO,WILLIAMS,YONEX";
+var _shoe = "未知,FOOTJOY,Adidas,Nike,ECCO,Mizuno,ECCO,Adidas,YONEX ,DUNLOP,TRAX,Oakley,NUMBER,TRUE linkswear,Nike,Mizuno,昕风采";
+
 
 Template.userinfo.created = function() {
   id = Router.current().params._id;
   if(!id){
     id = Meteor.userId();
   }
-  
   user = Meteor.users.findOne(id);
 };
 
@@ -39,16 +50,47 @@ Template.userinfo.helpers({
   Meteor.subscribe('userMatch', id,1,function(){
     
   });
-
   var match = matchData.findOne({userID:id},{ sort: {createdAt: -1}});
-
     if(match){
       return match;
     }
-  
  },
  checkFriend:function(){
+ },
+ getSex:function(){
+  return _sex.split(",")[user.profile.sex];
+ },
+ getStatus:function(){
+  return _status.split(",")[user.profile.sex];
+ },
+ getPosition:function(){
+  return _position.split(",")[user.profile.position];
+ },
+ getProfession:function(){
+  return _profession.split(",")[user.profile.profession];
+ },
+ getDriver:function(){
+  return _driver.split(",")[user.profile.driver];
+ },
+ getFairwayWood:function(){
+  return _fairway_wood.split(",")[user.profile.fairway_wood];
+ },
+ getHybrid:function(){
+  return _hybrid.split(",")[user.profile.hybrid];
+ },
+ getIrons:function(){
+  return _irons.split(",")[user.profile.irons];
+ },
+ getWedges:function(){
+  return _wedges.split(",")[user.profile.wedges];
+ },
+ getPutter:function(){
+  return _putter.split(",")[user.profile.putter];
+ },
+ getShoe:function(){
+  return _shoe.split(",")[user.profile.shoe];
  }
+
 });
 
 Template.userinfo.events({
@@ -75,6 +117,4 @@ Template.userinfo.events({
     }
 
   },
- 
-
 });
