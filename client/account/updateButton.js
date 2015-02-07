9aca2f1df1263bdcdb1c5b3bf689ad3949d82f80
pyunqi <ypang@tibco-support.com>
2015-02-07 14:09:26 +0800

    球龄计算加入

--- client/account/updateButton.js
+++ client/account/updateButton.js
@@ -1,12 +1,23 @@
 
 Template.updateButton.events({
  'click #update-profile': function () {
+      Session.set(ERROR_MESSAGE,'');
       var nick_name=$("#nick-name").val();
       if(nick_name.length < 2){
         Session.set(ERROR_MESSAGE, "用户名不能少于2个字");
         return;
       }
-      var ball_age=$("#ball-age").val();
+      var ball_age_years=$("#ball-age").val();
+      if(isNaN(ball_age_years)){
+        Session.set(ERROR_MESSAGE, "球龄必须是数字");
+        document.getElementById("ball-age").focus();
+        return;
+      }
+      if(ball_age_years > 99){
+        Session.set(ERROR_MESSAGE, "我们只接受球龄小于99年的人士...ORZ");
+        return;
+      }
+      var ball_age = moment().add('years',-ball_age_years).unix();
       var sex = $("#sel-sex").val();
       var status = $("#sel-status").val();
       var position = $("#sel-position").val();
@@ -20,7 +31,7 @@ Template.updateButton.events({
       var wedges = $("#sel-wedges").val();
       var putter = $("#sel-putter").val();
       var shoe = $("#sel-shoe").val();
-    Meteor.users.update({_id: Meteor.userId()},
+      Meteor.users.update({_id: Meteor.userId()},
       {$set: {'profile.nick_name': nick_name, 'profile.sex': sex, 'profile.status': status,
               'profile.ball_age': ball_age, 'profile.position': position, 'profile.profession': profession,
               'profile.company': company,'profile.des': des,'profile.driver': driver,
